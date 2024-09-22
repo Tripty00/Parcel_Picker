fb3c45000f21073fa2afd5f0e48f115b14dc0040
DESKTOP-30GPAHA\User <suriasarmintripty@gmail.com>
2024-09-22 08:06:42 +0600

    SignUp

--- SignUp.php
+++ SignUp.php
@@ -1,25 +1,15 @@
-
-
-
 <?php
-
-@include 'Connection.php';
-
+include 'Connection.php';
 if(isset($_POST['submit'])){
-
    $name = $_POST['name'];
    $email = $_POST['email'];
-
    $address = $_POST['address'];
    $phn = $_POST['number'];
    $phn2 = $_POST['number2'];
    $password = $_POST['password'];
    $confirmpassword = $_POST['confirmpassword'];
-
    $select = " SELECT * FROM users WHERE email = '$email' ";
-
    $result = mysqli_query($conn, $select);
-
    if(mysqli_num_rows($result) > 0){
       $error[] = 'User already exist!';
    }else{
@@ -35,17 +25,14 @@ if(isset($_POST['submit'])){
          $conn->close();
       }
    };
-
 ?>
-
 <!DOCTYPE html>
 <html lang="en">
 <head>
    <title>Sign Up form</title>
    <link rel="stylesheet" href="style2.css">
 </head>
- <!-- <body style="background: url(img5.jpeg);background-repeat:no-repeat;background-size:100%">    -->
- <body style="background: Purple">    
+ <body style="background: url('img7.jpeg') no-repeat center center fixed; background-size: cover;">  
 <div class="form-container">
 
    <form action="" method="post">
@@ -63,14 +50,12 @@ if(isset($_POST['submit'])){
       <input type="text" name="number" required placeholder="Enter your phone number">
       <input type="text" name="number2" placeholder="Enter your phone number(optional)">
       <input type="password" name="password" required placeholder="Enter your password">
-      <input type="password" name="cpassword" required placeholder="Confirm your password">
+      <input type="password" name="confirmpassword" required placeholder="Confirm your password">
       <input type="submit" name="submit" value="Sign Up" class="form-btn">
-      <p>Already have an account? <a href="LoginPage.php">Log in</a></p>
-
+      <p>Already have an account? <a href="LoginPage.php">Log in</a></p><br>
+      <p>Are you  a rider? Sign Up  <a href="RiderSignUp.php">Here</a></p>
    </form>
-
 </div>
-
 </body>
 </html>
  
