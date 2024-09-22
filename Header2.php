02232e1bc7c420eb1c35f0e9c135d6c1e9251da3
DESKTOP-30GPAHA\User <suriasarmintripty@gmail.com>
2024-09-22 08:05:14 +0600

    Header File

---
+++ Header2.php
@@ -0,0 +1,46 @@
+<?php include 'Connection.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" 
+    integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">   
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="Parcel_Picker.css" />
+    <link href="https://fonts.googleapis.com/css2?family=Pacifico&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.5.2/css/all.min.css">
+    <link rel="stylesheet" href="Parcel_Picker.css" />
+    <title>Parcel Picker</title>
+</head>
+<body style="background: Purple"> 
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+        <div class="container">
+          <a class="navbar-brand" href="#">
+            <img src="Images/Logo.png" alt="logo" class="logo">
+          </a>
+          <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+          </button>
+          <div class="collapse navbar-collapse" id="navbarSupportedContent">
+            <ul class="navbar-nav ms-auto mb-2 mg-lg-0">
+              <li class="nav-item">
+                <a class="nav-link" aria-current="page" href="RiderHomePage.php">Home</a>
+              </li>
+              <li class="nav-item">
+                <a class="nav-link" href="ParcelList.php">Parcel List</a>
+              </li>             
+              <li class="nav-item">
+                <a class="nav-link" href="Contact2.php">Contact Us</a>
+              </li>
+            </ul>
+            <button class="button" onclick="location.href='Logout.php'">Logout</button>
+          </div>
+        </div>
+      </nav>
+      <div class="main_div">
+      </div>
+</body>
+</html>
+
+
