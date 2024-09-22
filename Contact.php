9028a2ce733965358653747cbcaa1dc84f58de31
DESKTOP-30GPAHA\User <suriasarmintripty@gmail.com>
2024-09-22 07:55:09 +0600

    Contact

---
+++ Contact.php
@@ -0,0 +1,26 @@
+<?php include 'Connection.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <title>Parcel Picker</title>
+</head>
+<body style="background: Purple"> 
+<nav class="navbar navbar-expand-lg navbar-light fixed-top">
+            <?php include 'Header.php'; ?>
+            <div class="carousel-caption carousel1">
+
+                <p>Contact Us</p>
+                <p>If you find any problem to send courier.Please contact us.</p>
+                <p>Email: parcelpicker@gmail.com</p>
+                <p>Phone: +8801234567890</p>
+                <h5>Follow Us</h5>
+                    <div class="social-links">
+                        <a href="#"><i class="fab fa-facebook-f"></i></a>
+                        <a href="#"><i class="fab fa-twitter"></i></a>
+                        <a href="#"><i class="fab fa-instagram"></i></a>
+                        <a href="#"><i class="fab fa-linkedin"></i></a>
+                </div>          
+      <div class="main_div">     
+      </div>
+</body>
+</html>
